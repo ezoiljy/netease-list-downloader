3c8b7a34fc3fff07b6e4f9ac63000b91133a7053
xukl <xu2k3l4@outlook.com>
2018-08-19 18:57:04 +0800

    小修改download.sh
    给mkdir加了'-p'，cd到目标文件夹来简化后续操作

--- download.sh
+++ download.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo '请输入歌单id以及目标下载位置（目录请避免~符号）'
 read id path
-mkdir $path/$id
+mkdir -p "$path/$id"
+cd "$path/$id"
 
 node download.js $id > log
 while read name url; do
-    wget -b "$url" -O "$path/$id/$name.mp3"
+    wget -b "$url" -O "$name.mp3"
 done < log
 rm log
